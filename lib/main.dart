a3900b9f8b614c441eadd58b9df75b09544b8f58
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-02-20 21:07:59 -0600

    Se cambio el home.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-// import 'package:animate_do_app/src/pages/page1_page.dart';
-import 'package:animate_do_app/src/pages/twitter_page.dart';
+import 'package:animate_do_app/src/pages/page1_page.dart';
  
 void main() => runApp(MyApp());
  
@@ -11,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'animate_do App',
-      home: TwitterPage(),
+      home: Page1Page(),
     );
   }
 }
