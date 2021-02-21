93b6ec6516f03b904644324f7cbf93dff27856a7
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-02-20 19:18:14 -0600

    Se creo un nueva pagina.

---
+++ lib/src/pages/page1_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Page1Page extends StatelessWidget {
+  const Page1Page({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Page 1 Page'),
+      ),
+    );
+  }
+}
