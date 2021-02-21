b42571107d52cf5e2195d487ac0c0b406af1914b
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-02-20 19:18:25 -0600

    Se cambio el home.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import 'package:animate_do_app/src/pages/page1_page.dart';
  
 void main() => runApp(MyApp());
  
@@ -6,17 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'animate_do App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('animate_do App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      home: Page1Page(),
     );
   }
 }
